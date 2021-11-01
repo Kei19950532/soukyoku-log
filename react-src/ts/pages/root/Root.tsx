fd9e5c66956c2643dd4f768a1340de332575d459
kei <66989217+Kei19950532@users.noreply.github.com>
2021-11-01 09:06:50 +0000

    Add: Rootページの追加

--- react-src/ts/pages/root/Root.tsx
+++ react-src/ts/pages/root/Root.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 
-import { Root } from "@/pages";
-
-const App: React.VFC = () => {
+export const Root: React.VFC = () => {
     const title: string = "TypeScript React !!";
     return <h1>{title}</h1>;
 };
-
-export default App;
