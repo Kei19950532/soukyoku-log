ca54f2e39bca62b14b0d9ad22a8fd542cf873634
kei <66989217+Kei19950532@users.noreply.github.com>
2021-11-02 07:01:16 +0000

    トップページのHTML土台の追加

--- react-src/ts/App.tsx
+++ react-src/ts/App.tsx
@@ -3,8 +3,7 @@ import React from "react";
 import { Root } from "@/pages";
 
 const App: React.VFC = () => {
-    const title: string = "TypeScript React !!";
-    return <h1>{title}</h1>;
+    return <Root />;
 };
 
 export default App;
